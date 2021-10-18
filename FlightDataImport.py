c576f4bd72c319d318f420f51bdc31e11f41cf2c
JewishStew <70977089+ZackHagerty@users.noreply.github.com>
2021-10-18 17:48:26 -0400

    Fuckhead McGee

--- FlightDataImport.py
+++ FlightDataImport.py
@@ -7,20 +7,25 @@ import requests
 import json
 import csv
 
+class FlightData():
 
-#Latitude and Longitude trimming from OpenSky, to be changed later
-longmin, latmin = -130.974, 20.038
-longmax, latmax = -65.748, 50.214
+    def __init__(self):
+        # Latitude and Longitude trimming from OpenSky, to be changed later
+        longmin, latmin = -130.974, 20.038
+        longmax, latmax = -65.748, 50.214
+        self.flightDataPull(latmin, longmin, longmax, latmax)
 
-#Pull Request from browser search of opensky using lat and long data
-OpenSky_datapull = 'https://opensky-network.org/api/states/all?lamin=' + str(latmin) + '&lomin=' + str(longmin) + '&lamax=' + str(latmax) + '&lomax=' + str(longmax)
-OpenSky_data = requests.get(OpenSky_datapull).json()
-
-
-columns = ['icao24','callsign','origin_country','time_position','last_contact','long','lat','baro_altitude','on_ground (T/F)','velocity','true_track','vertical_rate','sensors',
-'geo_altitude','squawk','spi','position_source']
-flight_df=pd.DataFrame(OpenSky_data['states'],columns=columns)
-flight_df=flight_df.fillna('No Data')
+    def flightDataPull(self, latmin, longmin, longmax, latmax):
+        #Pull Request from browser search of opensky using lat and long data
+        OpenSky_datapull = 'https://opensky-network.org/api/states/all?lamin=' + str(latmin) + '&lomin=' + str(longmin) + '&lamax=' + str(latmax) + '&lomax=' + str(longmax)
+        OpenSky_data = requests.get(OpenSky_datapull).json()
 
 
-flight_df.to_csv('icons.csv')
+
+        columns = ['icao24','callsign','origin_country','time_position','last_contact','long','lat','baro_altitude','on_ground (T/F)','velocity','true_track','vertical_rate','sensors',
+        'geo_altitude','squawk','spi','position_source']
+        flight_df=pd.DataFrame(OpenSky_data['states'],columns=columns)
+        flight_df=flight_df.fillna('No Data')
+
+
+        flight_df.to_csv('icons.csv')
--- GUI.py
+++ GUI.py
@@ -2,26 +2,36 @@ from tkinter import *
 from tkinter import ttk
 from PIL import ImageTk, Image
 
-root = Tk()
-USAMapImage = ImageTk.PhotoImage(file = './U.S.A Images/Untitled.png')
-USAMap = Label(image = USAMapImage)
+from FlightDataImport import FlightData
 
-USAMap.pack()
-root.geometry("1114x642")
-root.title("Flight Tracking")
-root.mainloop()
+class GUI():
 
+    root = Tk()
+    USAMapImage = ImageTk.PhotoImage(file = './U.S.A Images/Untitled.png')
+    USAMap = Label(image = USAMapImage)
 
-# Matthew Writes a Method
-# Given N and W, will give you X and Y pixels for the GUI
-def coordinateTranslate(N, W):
+    FlightData = FlightData()
+    USAMap.pack()
+    root.geometry("1114x642")
+    root.title("Flight Tracking")
+    root.mainloop()
+
     
-    # Based on photo from Zack:
-    x = (130-65)/1114 * (W + 130)
-    y = -(642/30) * (N - 50)
+    # Matthew Writes a Method
+    # Given N and W, will give you X and Y pixels for the GUI
 
-    return x,y
 
+    def coordinateTranslate(N, W):
+        
+        # Based on photo from Zack:
+        x = (130-65)/1114 * (W + 130)
+        y = -(642/30) * (N - 50)
+
+        return x,y
+
+if __name__ == "__main__":
+    
+    Main = GUI()
 
 
 
