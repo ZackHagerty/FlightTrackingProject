0ff256e1b1a9a5ce2c721cab608ec6bf5c2a3e9a
Robo <91501991+RoboDog714@users.noreply.github.com>
2021-10-17 20:26:03 -0400

    Update FlightDataImport.py
    Heck

--- FlightDataImport.py
+++ FlightDataImport.py
@@ -13,10 +13,12 @@ longmax, latmax = -68.748, 52.214
 #Pull Request from browser search of opensky using lat and long data
 OpenSky_datapull = 'https://opensky-network.org/api/states/all?lamin=' + str(latmin) + '&lomin=' + str(longmin) + '&lamax=' + str(latmax) + '&lomax=' + str(longmax)
 OpenSky_data = requests.get(OpenSky_datapull).json()
+response=requests.get(OpenSky_data).json()
+
 
 columns = ['icao24','callsign','origin_country','time_position','last_contact','long','lat','baro_altitude','on_ground (T/F)','velocity','true_track','vertical_rate','sensors',
 'geo_altitude','squawk','spi','position_source']
-flight_df=pd.DataFrame(Response['states'],columns=columns)
+flight_df=pd.DataFrame(response['states'],columns=columns)
 flight_df=flight_df.fillna('No Data')
 
-flight_df.to_csv
+flight_df.to_csv(filepath = "./icons", mode = "w")
