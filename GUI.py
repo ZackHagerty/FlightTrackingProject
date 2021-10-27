42e6fab66da06217f77fcfc5a3770665e8b6a2ab
MattJCoding <91501820+MattJCoding@users.noreply.github.com>
2021-10-27 17:26:21 -0400

    atc zoned

--- GUI.py
+++ GUI.py
@@ -61,7 +61,7 @@ class GUI():
         #img_ref.append(plane3done)
     
                                                 # (latmin, longmin, longmax, latmax)
-    flight_chart_array = FlightData.flightDataPull(24,-87,-79,31)
+    flight_chart_array = FlightData.flightDataPull(24,-80,-79,30)
 
     #true_Array = datafile['true_track'].tolist()
     #long_Array = datafile['long'].tolist()
@@ -94,9 +94,100 @@ class GUI():
     #root.title("Flight Tracking")
     #AirplaneIcon.place(x=100,y=100)
 
+
+
+    # black only atc zones
+    albuquerqueATC = Image.open('./atc_zones/albuquerque_zone_black.png')
+    albuquerqueATCMap = ImageTk.PhotoImage(albuquerqueATC)
+    canvas.create_image(150,100,image=albuquerqueATCMap)
+
+    atlantaATC = Image.open('./atc_zones/atlanta_zone_black.png')
+    atlantaATCMap = ImageTk.PhotoImage(atlantaATC)
+    canvas.create_image(150,100, image = atlantaATCMap)
+    
+    bostonATC = Image.open('./atc_zones/boston_zone_black.png')
+    bostonATCMap = ImageTk.PhotoImage(bostonATC)
+    canvas.create_image(150,100, image = bostonATCMap)
+    
+    chicagoATC = Image.open('./atc_zones/chicago_zone_black.png')
+    chicagoATCMap = ImageTk.PhotoImage(chicagoATC)
+    canvas.create_image(150,100, image = chicagoATCMap)
+    
+    clevelandATC = Image.open('./atc_zones/cleveland_zone_black.png')
+    clevelandATCMap = ImageTk.PhotoImage(clevelandATC)
+    canvas.create_image(150,100, image = clevelandATCMap)
+    
+    denverATC = Image.open('./atc_zones/denver_zone_black.png')
+    denverATCMap = ImageTk.PhotoImage(denverATC)
+    canvas.create_image(150,100, image = denverATCMap)
+    
+    ft_worthATC = Image.open('./atc_zones/ft_worth_zone_black.png')
+    ft_worthATCMap = ImageTk.PhotoImage(ft_worthATC)
+    canvas.create_image(150,100, image = ft_worthATCMap)
+    
+    houstonATC = Image.open('./atc_zones/houston_zone_black.png')
+    houstonATCMap = ImageTk.PhotoImage(houstonATC)
+    canvas.create_image(150,100, image = houstonATCMap)
+    
+    indianapolisATC = Image.open('./atc_zones/indianapolis_zone_black.png')
+    indianapolisATCMap = ImageTk.PhotoImage(indianapolisATC)
+    canvas.create_image(150,100, image = indianapolisATCMap)
+    
+    jacksonvilleATC = Image.open('./atc_zones/jacksonville_zone_black.png')
+    jacksonvilleATCMap = ImageTk.PhotoImage(jacksonvilleATC)
+    canvas.create_image(150,100, image = jacksonvilleATCMap)
+    
+    kansas_cityATC = Image.open('./atc_zones/kansas_city_zone_black.png')
+    kansas_cityATCMap = ImageTk.PhotoImage(kansas_cityATC)
+    canvas.create_image(150,100, image = kansas_cityATCMap)
+    
+    los_angelesATC = Image.open('./atc_zones/los_angeles_zone_black.png')
+    los_angelesATCMap = ImageTk.PhotoImage(los_angelesATC)
+    canvas.create_image(150,100, image = los_angelesATCMap)
+
+    memphisATC = Image.open('./atc_zones/memphis_zone_black.png')
+    memphisATCMap = ImageTk.PhotoImage(memphisATC)
+    canvas.create_image(150,100, image = memphisATCMap)
+
+    miamiATC = Image.open('./atc_zones/miami_zone_black.png')
+    miamiATCMap = ImageTk.PhotoImage(miamiATC)
+    canvas.create_image(840,500, image = miamiATCMap)
+
+    minnealpolisATC = Image.open('./atc_zones/minnealpolis_zone_black.png')
+    minnealpolisATCMap = ImageTk.PhotoImage(minnealpolisATC)
+    canvas.create_image(150,100, image = minnealpolisATCMap)
+
+    new_yorkATC = Image.open('./atc_zones/new_york_zone_black.png')
+    new_yorkATCMap = ImageTk.PhotoImage(new_yorkATC)
+    canvas.create_image(150,100, image = new_yorkATCMap)
+
+    oaklandATC = Image.open('./atc_zones/oakland_zone_black.png')
+    oaklandATCMap = ImageTk.PhotoImage(oaklandATC)
+    canvas.create_image(150,100, image = oaklandATCMap)
+
+    salt_lakeATC = Image.open('./atc_zones/salt_lake_zone_black.png')
+    salt_lakeATCMap = ImageTk.PhotoImage(salt_lakeATC)
+    canvas.create_image(150,100, image = salt_lakeATCMap)
+
+    seattleATC = Image.open('./atc_zones/seattle_zone_black.png')
+    seattleATCMap = ImageTk.PhotoImage(seattleATC)
+    canvas.create_image(150,100, image = seattleATCMap)
+
+    washingtonATC = Image.open('./atc_zones/washington_zone_black.png')
+    washingtonATCMap = ImageTk.PhotoImage(washingtonATC)
+    canvas.create_image(150,100, image = washingtonATCMap)
+    
+
+    # put in all of the black ones in their correct places
+
+
+
+
+
+
     root.mainloop()
 
-
+    
 
 
 
