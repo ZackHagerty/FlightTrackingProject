38f78be1d98ab36cb7badefcadddf9f8531af218
Robo <91501991+RoboDog714@users.noreply.github.com>
2021-11-29 17:00:11 -0500

    Update Nuking Grounds.py

--- Nuking Grounds.py
+++ Nuking Grounds.py
@@ -25,4 +25,18 @@ def flightDataPull(latmin, longmin, longmax, latmax):
         user_name = 'zack.hagerty'
         password = 'SE300'
         OpenSky_datapull = 'https://'+user_name+':'+password+'@opensky-network.org/api/states/all?lamin=' + str(latmin) + '&lomin=' + str(longmin) + '&lamax=' + str(latmax) + '&lomax=' + str(longmax)
-        OpenSky_data = requests.get(OpenSky_datapull).json()
+        OpenSky_data = requests.get(OpenSky_datapull)
+
+        try:
+            columns = ['icao24','callsign','origin_country','time_position','last_contact','long','lat','baro_altitude','on_ground (T/F)','velocity','true_track','vertical_rate','sensors',
+            'geo_altitude','squawk','spi','position_source','unknown']
+            flight_df=pd.DataFrame(OpenSky_data['states'],columns=columns)
+            flight_df=flight_df.fillna('No Data')
+        except ValueError:
+            columns = ['icao24','callsign','origin_country','time_position','last_contact','long','lat','baro_altitude','on_ground (T/F)','velocity','true_track','vertical_rate','sensors',
+            'geo_altitude','squawk','spi','position_source']
+            flight_df=pd.DataFrame(OpenSky_data['states'],columns=columns)
+            flight_df=flight_df.fillna('No Data')
+
+        print(flight_df)
+        
